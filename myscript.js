358a76d7439c320964e34d149730b2759c71925b
Kyle Loftin <kaloftin85@gmail.com>
2022-04-15 13:56:16 -0700

    started slidebar for sizing

--- myscript.js
+++ myscript.js
@@ -1,24 +1,42 @@
-const DIVNUM = 40;
 const CONTAINER = document.querySelector('.container');
 const RESET = document
   .getElementById('reset-button')
   .addEventListener('click', resetButton);
+let divNum = 40;
 
-for (let i = 0; i < DIVNUM; i++) {
-  const COLUMNDIV = document.createElement('div');
-  COLUMNDIV.className = 'column-div';
-  CONTAINER.appendChild(COLUMNDIV);
+function inputFunction(val) {
+  divNum = val;
+  document.getElementById('boxValue').innerHTML = `${val} x ${val}`;
+  resetButton();
+}
 
-  for (let j = 0; j < DIVNUM; j++) {
-    const ROWDIV = document.createElement('div');
-    ROWDIV.className = 'row-div';
-    ROWDIV.addEventListener('mouseover', function (e) {
-      e.target.style.backgroundColor = 'red';
-    });
-    COLUMNDIV.appendChild(ROWDIV);
+function clearBoard(num = 40, checkBoard = false) {
+  if (checkBoard) {
+    while (CONTAINER.firstChild) {
+      CONTAINER.removeChild(CONTAINER.firstChild);
+    }
+  }
+
+  document.getElementById('boxValue').innerHTML = `${num} x ${num}`;
+
+  for (let i = 0; i < num; i++) {
+    const COLUMNDIV = document.createElement('div');
+    COLUMNDIV.className = 'column-div';
+    CONTAINER.appendChild(COLUMNDIV);
+
+    for (let j = 0; j < num; j++) {
+      const ROWDIV = document.createElement('div');
+      ROWDIV.className = 'row-div';
+      ROWDIV.addEventListener('mouseover', function (e) {
+        e.target.style.backgroundColor = 'red';
+      });
+      COLUMNDIV.appendChild(ROWDIV);
+    }
   }
 }
 
 function resetButton() {
-  alert('Yo');
+  clearBoard(divNum, true);
 }
+
+clearBoard(divNum);
