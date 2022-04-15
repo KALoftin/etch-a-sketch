9a6ba15758c22fa6190685a26dc98907e4c7c759
Kyle Loftin <kaloftin85@gmail.com>
2022-04-15 00:14:42 -0700

    cleaned up code

--- myscript.js
+++ myscript.js
@@ -1,14 +1,22 @@
-const container = document.querySelector('.container');
-const reset = document
+const DIVNUM = 40;
+const CONTAINER = document.querySelector('.container');
+const RESET = document
   .getElementById('reset-button')
   .addEventListener('click', resetButton);
 
-for (let i = 0; i < 256; i++) {
-  const gridDivs = document.createElement('div');
-  gridDivs.addEventListener('mouseover', function (e) {
-    e.target.style.backgroundColor = 'red';
-  });
-  container.appendChild(gridDivs);
+for (let i = 0; i < DIVNUM; i++) {
+  const COLUMNDIV = document.createElement('div');
+  COLUMNDIV.className = 'column-div';
+  CONTAINER.appendChild(COLUMNDIV);
+
+  for (let j = 0; j < DIVNUM; j++) {
+    const ROWDIV = document.createElement('div');
+    ROWDIV.className = 'row-div';
+    ROWDIV.addEventListener('mouseover', function (e) {
+      e.target.style.backgroundColor = 'red';
+    });
+    COLUMNDIV.appendChild(ROWDIV);
+  }
 }
 
 function resetButton() {
